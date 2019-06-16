a5f0932754b35da880f4eefd4124d6704ab24e31
lixuhui123 <2871726754@qq.com>
2019-06-16 23:14:43 +0800

    对文件的一些操作

--- file take/file.cpp
+++ file take/file.cpp
@@ -2,7 +2,13 @@
 #include<stdlib.h>
 int main()
 {
-	typedef  
+	int a = 1000;
+	FILE *fp = NULL;
+	fp = fopen("C:\\Users\\Z7M\\Desktop\\2.txt", "wb");
+	fwrite(&a, 4, 1, fp);
+	fread()
+	fclose(fp);
+	fp = NULL;
 	system("pause");
 	return 0;
 }
